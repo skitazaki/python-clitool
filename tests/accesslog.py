da00d365c30d15c5ccef2be73763fd0ba581ccac
Shigeru Kitazaki <skitazaki@gmail.com>
2014-07-13 00:09:16 +0900

    Add timezone "utcoffset" support on `parse` function.

--- tests/accesslog.py
+++ tests/accesslog.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datetime
 from clitool.accesslog import parse
 
 
@@ -21,5 +22,59 @@ def test_accesslog_regex():
         print('Failed inputs:\n')
         print('\t', '\n\t'.join(error))
         assert False, "see above outputs"
+
+
+# See official document about "tzinfo" class.
+ZERO = datetime.timedelta(0)
+
+
+class UTC(datetime.tzinfo):
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def dst(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+
+class JST(datetime.tzinfo):
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(hours=9)
+
+    def dst(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "JST"
+
+
+def test_accesslog_timezone():
+    t = '''
+127.0.0.1 - - [22/Aug/2011:10:02:03 +0900] "GET /favicon.ico HTTP/1.1" 404 168 "-" "Mozilla/5.0 (Windows NT 5.1; rv:6.0) Gecko/20100101 Firefox/6.0"
+127.0.0.1 - - [22/Aug/2011:10:02:03 -0900] "GET /favicon.ico HTTP/1.1" 404 168 "-" "Mozilla/5.0 (Windows NT 5.1; rv:6.0) Gecko/20100101 Firefox/6.0"
+'''.strip().split('\n')
+    # Parse and test first line.
+    e = parse(t[0])
+    assert e['time'] == datetime.datetime(2011, 8, 22, 10, 2, 3)
+    assert e['utcoffset'] == datetime.timedelta(hours=9)
+    assert e['utcoffset'].total_seconds() == 9 * 3600
+    jst = JST()
+    utc = UTC()
+    # make datetime object "naive" to "aware".
+    d = e['time'].replace(tzinfo=jst)
+    assert d.day == 22
+    assert d.hour == 10
+    u = d.astimezone(utc)
+    assert u.day == 22
+    assert u.hour == 1  # 1 hour = 10 - 9
+    # Parse and test second line.
+    e = parse(t[1])
+    assert e['time'] == datetime.datetime(2011, 8, 22, 10, 2, 3)
+    assert e['utcoffset'] == datetime.timedelta(hours=-9)
+    assert e['utcoffset'].total_seconds() == -9 * 3600
 
 # vim: set et ts=4 sw=4 cindent fileencoding=utf-8 :
